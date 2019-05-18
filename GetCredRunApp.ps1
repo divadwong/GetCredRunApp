75622572b8ec13c000eef041aa4c8532903960bf
divadwong <40673755+divadwong@users.noreply.github.com>
2019-05-18 10:44:40 -0400

    Update GetCredRunApp.ps1

--- GetCredRunApp.ps1
+++ GetCredRunApp.ps1
@@ -1,16 +1,33 @@
 # GetCredRunApp.ps1
 # Created 5/9/19 - David Wong
+# Updated 5/18/19
 # Get credential from user and run Program with those credentials
 Param(
 	[Parameter(Mandatory=$False)]
 	[string]$RunExe=$null,
 
 	[Parameter(Mandatory=$False)]
-	[string]$LogonTitle="Logon Page",
+	[string]$LogonTitle="Login ",
 	
 	[Parameter(Mandatory=$False)]
-	[string]$LogAppName=$null
+	[array]$CheckGroup=$null
 )
+
+#  Popup Message function
+function PopupMsg($Msg,$PopupTitle,$Timeout,$Icon)
+{
+    $vbOKOnly=0;$vbOKCancel=1;$vbYesNoCancel=3;$vbYesNo=4
+    $VBCritical=16;$vbQuestion=32;$vbExclamation=48;$vbInformation=64
+    $vbDefaultButton1=0;$vbDefaultButton2=256;$vbDefaultButton3=512
+    $vbOK=1;$vbCancel=2;$vbAbort=3;$vbRetry=4;$vbIgnore=5;$vbYes=6;$vbNo=7
+    $DefaultButton=$vbDefaultButton1
+    $wshell = New-Object -ComObject Wscript.Shell
+    $Result = $wshell.Popup($Msg,$TimeOut,$PopupTitle,$vbOKOnly+$Icon+$vbDefaultButton)
+    if ($Result -eq $vbOK -or $Result -eq -1)
+        {return $True}
+    elseif ($Result -eq $vbCancel)
+        {return $false}
+}
 
 function Test-Credential {
     <# 
@@ -35,7 +52,7 @@ function Test-Credential {
     )
     begin {
         Add-Type -assemblyname system.DirectoryServices.accountmanagement
-        $DS = New-Object System.DirectoryServices.AccountManagement.PrincipalContext([System.DirectoryServices.AccountManagement.ContextType]::$context) 
+        $DS = New-Object System.DirectoryServices.AccountManagement.PrincipalContext([System.DirectoryServices.AccountManagement.ContextType]::$context)
     }
     process {
         $DS.ValidateCredentials($credential.UserName, $credential.GetNetworkCredential().password)
@@ -45,24 +62,58 @@ function Test-Credential {
 ### Start ####
 $location = Split-Path $PSCommandPath -Parent
 $Server=$env:computername
+$Domain=$env:userdomain
 
 # If no -RunEXE parameters, log error and quit.
 if (!($RunEXE)){Exit}	
 
 # Get and check credentials ($Credential) from user.
-$Authenticated = $false
 $LogonMess = " Please enter your User name and Password"
 
 # Get Credentials until validated or canceled (will exit script).
 Do{
-    $Credential = $host.ui.PromptForCredential($LogonTitle, $LogonMess , "", "yourdomainhere")
+    $Credential = $host.ui.PromptForCredential($LogonTitle, $LogonMess , "", $Domain)
 	if (!($Credential)){Exit}
-	$Authenticated = Test-Credential $credential
-	$LogonMess = " Invalid Login. Please try again"
-}
-While(!($Authenticated))	
 
-$RunExeArg = "-File \\Server\Share\Folder\RunAppUsingCred.ps1 -RunEXE `"$RunEXE`""
+	$UserID = $Credential.Username
+	if ($UserID.Split('\').count-1 -gt 0){
+		if ($UserID.Split('\')[0] -eq $Domain){$GoodDomain = $true;$UserID = ($Credential.username).Split('\')[1]} 
+		else {$GoodDomain = $false}
+	}	
+	elseif ($UserID.Split('@')[1] -eq $Domain){$GoodDomain = $true;$UserID = ($Credential.username).Split('@')[0]}
+		else {$GoodDomain = $false}
+	
+	if ($GoodDomain){
+		$Authenticated = Test-Credential $credential
+		$LogonMess = " Invalid Login. Please try again"
+	}
+	else {
+		$Authenticated = $false
+		$LogonMess = "Please type only User name and exclude domain"
+	}
+} While(!$Authenticated)	
+
+$RunExeArg = "-File \\server\Share\RunGCrApp.ps1 -RunEXE `"$RunEXE`""
 
 # Run EXE with Credentials gathered
-if ($Credential) {Start-Process Powershell.exe $RunExeArg -Credential $Credential -WindowStyle Hidden}
+if ($Credential)
+{	
+	$InCheckedGroup = $false	# Initialize
+	
+	# & X:\Apps\Wait.exe  # Please wait message	
+	
+	if ($CheckGroup){
+		$InGroups = (Get-ADPrincipalGroupMembership $UserID).sAMAccountName
+		if ($InGroups -Contains $CheckGroup){$InCheckedGroup = $true}
+	}
+	else {$InCheckedGroup = $null}
+		
+	if (($InCheckedGroup -eq $true) -or ($InCheckedGroup -eq $null)){
+		Start-Process Powershell.exe $RunExeArg -Credential $Credential -WindowStyle Hidden
+	}
+	else
+	{
+		PopupMsg "You are not authorized to launch this application. Call your HelpDesk for assistance." "ERROR: Not in $CheckGroup Group" 30 16
+		$UserID = $UserID + "-NotInGroup-" + $CheckGroup
+	}
+}
